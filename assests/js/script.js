21e32485eabcc090a4442e704df80ce0c6f17983
Adam Weaver <adamweaver@live.co.uk>
2023-06-01 17:11:09 +0000

    add animation and js to flip playing cards

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,9 @@
-console.log("Connected!")
+// making card flip
+
+const cards = document.querySelectorAll('.card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
