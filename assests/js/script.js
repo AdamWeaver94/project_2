561f128095017c2211db56119b8312835627111b
Adam Weaver <adamweaver@live.co.uk>
2023-06-06 18:38:46 +0000

    add js rule for start buton intialising timer, debuging move inop

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,50 @@
-// inital game function
+// game selectors for js
+
+const moves = document.getElementById("moves");
+const timeValue = document.getElementById("timer");
+const startButton = document.getElementById("start-button");
+const gameContainer = document.querySelector("matching_game");
+const controls = document.querySelector("initial-startUp")
+
+// start game
+startButton.addEventListener("click", () => {
+    movesCount = 0;
+    seconds = 0;
+    minutes = 0;
+    interval = setInterval(timeGenerator, 1000);
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+});
+
+// initial timer
+let seconds = 0,
+    miutes = 0;
+
+// initial move counter
+let movesCount = 0,
+    winCount = 0;
+
+// for timer logic
+const timeGenerator = () => {
+    seconds += 1;
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+
+    // timer before start
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+// calculating moves
+const movesCounter = () => {
+    movesCount += 1;
+    moves.innerHTML = `<span>Moves</span>${movesCount}`;
+};
+
+
+// flipping cards
 
 const cards = document.querySelectorAll('.card');
 
@@ -23,7 +69,7 @@ function flipCard() {
     checkForMatch();
 }
 
-// matching card js
+// matching card
 
 function checkForMatch() {
     if (firstCard.dataset.framework === secondCard.dataset.framework) {
